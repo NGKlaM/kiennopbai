f7f8fcc6837872f889ecd6159e3f681828dfc3a1
lamnk <lamnkph28782@fpt.edu.vn>
2022-09-30 19:23:09 +0700

    dsffds

---
+++ src/SoNgichDao.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SoNgichDao {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int so,ND,bandau=0;
+        System.out.println("nhap so");
+        so=scanner.nextInt();
+        ND = so;
+        for (; so != 0; so /= 10) {
+            int phandu = so % 10;
+            bandau = bandau * 10 + phandu;
+        }
+        System.out.println(so == bandau ? "Số " + so + " là số ngược đảo " : "Số " + so +
+                " không phải là số ngược đảo ");
+    }
+}
+
---
+++ src/Sodao.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Sodao {
+            public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                while (true) {
+                    int num1, numND, resultND = 0;
+                    System.out.println("Mời bạn nhập vào số cần check ngược đảo: ");
+                    num1 = Integer.parseInt(sc.nextLine());
+
+                    numND = num1;
+                    while(numND != 0) {
+                        int phanDu = numND % 10;
+                        resultND = resultND * 10 + phanDu;
+                        numND /= 10;
+                    }
+                    System.out.println(num1 == resultND ? "Số " + num1 + " là số ngược đảo " : "Số " + num1 +
+                            " không phải là số ngược đảo ");
+                }
+            }
+        }
+
+
+
